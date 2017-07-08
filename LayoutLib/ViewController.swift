767ecd56d03bdbfb44de631c3331c2dfc5cd9526
Dima <gubatenkod@gmail.com>
2017-07-08 19:02:01 +0300

    Initial Commit

---
+++ LayoutLib/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LayoutLib
+//
+//  Created by Dima Gubatenko on 08.07.17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
